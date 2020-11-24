fb78321fb76f5ee1f9284f6a7d7eb5791b084ea1
James Bunker <56797822+JBunker66@users.noreply.github.com>
2020-11-24 15:39:54 +0000

    Update tunnel_detector.ino
    fixed some of the  MANY errors that I just let slide for some reason before

--- tunnel_detector.ino
+++ tunnel_detector.ino
@@ -11,13 +11,15 @@ Adafruit_DCMotor *motorL = AFMS.getMotor(1);
 Adafruit_DCMotor *motorR = AFMS.getMotor(2);
 
 //variable for line following
-  int vSpeed = 110;        // MAX 255
-  int turn_speed = 230;    // MAX 255 
+  int vSpeedRight = 155;        // MAX 255
+  int vSpeedLeft = 140;
+  int turn_speed = 230;
+  int turn_speed_offwheel = 100;// MAX 255 
   int turn_delay = 10;
 
 //OPB line Sensor Connection
-const int left_sensor_pin = A0;
-const int right_sensor_pin = A1;
+const int left_sensor_pin = 0;
+const int right_sensor_pin = 1;
 
 int left_sensor_state;
 int right_sensor_state;
@@ -30,12 +32,13 @@ boolean hasRedBlock = false;
 boolean hasBlueBlock = false;
 //robot on the left and use to check if robot has made the T turn after the tunnel on the right
 boolean leftSide = false;
+
 // Standard code above
 int tunnel_delay = 0;
 // Replace with real sensor
-int side_sensor = 36, side_sensor_history; // Arbitrary value
+int side_sensor_pin = 3, side_sensor_history; // Arbitrary value
 boolean Tunnel_near = false;
-unsigned long delayStart = 0; // the time the delay started
+unsigned long delayStart = 0, delayPause; // the time the delay started
 bool delayRunning = true; // true if still waiting for delay to finish
 int line_detector_history, tunnel_counter = 0;
 void setup() {
@@ -45,27 +48,28 @@ void setup() {
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
 
-  motorL->setSpeed(vSpeed);
-  motorR->setSpeed(vSpeed);
+  motorL->setSpeed(vSpeedLeft);
+  motorR->setSpeed(vSpeedRight);
   motorL->run(FORWARD);
-  motorR->run(FORWARD);
+  motorR->run(BACKWARD);
   // turn on motor
-  motorL->run(RELEASE);
-  motorR->run(RELEASE);
+  //motorL->run(RELEASE);
+  //motorR->run(RELEASE);
 
 
 // Standard code above
-  side_sensor_history = side_sensor;
-  delayStart = millis();   // start delay
+  side_sensor_history = analogRead(side_sensor_pin);
 }
 void loop() {
   // put your main code here, to run repeatedly:
   if (tunnel_delay = 10){// Dont want to check every loop so difference is clear, but short enough that corners dont trigger
-      if((side_sensor_history - side_sensor) < -450){ // Distance but might have to use voltage
+      if((side_sensor_history - analogRead(side_sensor_pin)) < -450){ // Distance but might have to use voltage
         Tunnel_near = true;
         delayRunning = true;
-        delayStart = 0;
+        delayStart = millis(); // Start delay
       }
+      side_sensor_history = analogRead(side_sensor_pin);
+      tunnel_delay = 0;
   }
   else{
     tunnel_delay += 1;
@@ -76,21 +80,24 @@ void loop() {
 }  
   if (hasBlueBlock && Tunnel_near && right_sensor_state == 0){ // Right detector sensing white
     // Reverse code here if needed. Assuming not
-    // Pause timer -- need to do
+    delayPause = millis();// Pause timer
     motorL->run(RELEASE);
     motorR->run(RELEASE);
     motorR->setSpeed(70);
     motorL->setSpeed(135);
     motorL->run(FORWARD);
     motorR->run(FORWARD);
-    line_detector_history = 1; // Actualy = digital read of line sensor
+    line_detector_history = digitalRead(1); // Actualy = digital read of line sensor
     for(int i = 0;  i < 40; i = i+1){
-       // 8 second delay roughly 90 degrees so go for 10
+       // 8 second delay roughly 90 degrees so go for 10 for 100 degree
        delay(250); // 4 times a second should be ok.
-       if(left_sensor_state != line_detector_history){ // current digital read here
+       if(digitalRead(1) != line_detector_history){ // Checks to see if state changes and if it does updates the history
          tunnel_counter +=1;
+         line_detector_history = digitalRead(1);
        }
        if(tunnel_counter == 3){
+        Tunnel_near = true;
+        delayRunning = true;
         break;
        }
     }
@@ -108,10 +115,11 @@ void loop() {
        delay(250); // 4 times a second should be ok.
        if(right_sensor_state != line_detector_history){ // current digital read here
          tunnel_counter +=1;
+         line_detector_history = digitalRead(left_sensor_pin); // - current digital read
        }
        if(tunnel_counter == 2){
+        delayStart = delayPause - delayStart;
         break;
-       line_detector_history = left_sensor_state; // - current digital read
        }
     }
     }
