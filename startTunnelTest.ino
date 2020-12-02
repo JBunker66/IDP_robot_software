079b488489962b5874c1538e55df198b99a1bfef
hhe233 <th601@cam.ac.uk>
2020-12-02 11:11:56 +0000

    update

--- startTunnelTest.ino
+++ startTunnelTest.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
+//#include <TimerOne.h>
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
@@ -9,17 +10,25 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Get motor in port 1 & 2
 Adafruit_DCMotor *motorL = AFMS.getMotor(1);
 Adafruit_DCMotor *motorR = AFMS.getMotor(2);
-
+Adafruit_DCMotor *pincerL = AFMS.getMotor(3);
+Adafruit_DCMotor *pincerR = AFMS.getMotor(4);
 //variable for line following
   int vSpeedRight = 115;        // MAX 255
   int vSpeedLeft = 100;
   int turn_speed = 240;
+  int slow_speed = 70;
   int turn_speed_offwheel = 30;// MAX 255 
   int turn_delay = 10;
+
+// Variables for pincers
+  int pincer_speed = 80;
+  int pincer_delay = 1000;
 
 //OPB line Sensor Connection
 const int left_sensor_pin = A0;
 const int right_sensor_pin = A1;
+
+
 
 int left_sensor_state;
 int right_sensor_state;
@@ -35,7 +44,7 @@ boolean hasRedBlock = false;
 //robot has the blue block
 boolean hasBlueBlock = false;
 //robot on the left and use to check if robot has made the T turn after the tunnel on the right
-boolean leftSide = true;
+boolean leftSide = false;
 
 //could be used to determine whether the robot is lost, inAction++ if no if statement is matched in a loop;
 int noAction = 0;
@@ -49,6 +58,29 @@ unsigned long delayStart = 0, delayPause; // the time the delay started
 bool delayRunning = true; // true if still waiting for delay to finish
 int line_detector_history, tunnel_counter = 0;
 
+const int Front_sensor_pin = 8; // Set to real pin
+int distanceBlock;
+
+// Sensors for colour test
+int sensorPin0 = A0;    // select the input pin for the potentiometer
+int sensorValue0 = 0;  // variable to store the value coming from the sensor
+int sensorPin1 = A1;    // select the input pin for the potentiometer
+int sensorValue1 = 0;  // variable to store the value coming from the sensor
+int sensorPin2 = A2;    // select the input pin for the potentiometer
+int sensorValue2 = 0;  // variable to store the value coming from the sensor
+int red_blue = 0;
+
+// LED variables
+const int amber_pin = 12, red_pin = 10,blue_pin = 11;
+const int RED = 1, BLUE = 2, AMBER = 3;
+int current_LED = 12;
+unsigned long two_hz_delay = 0;
+boolean AmberON = false, On = true, Off = false;
+
+//ultrasonic sensor pin
+const int trigPin = 9;
+const int echoPin = 10;
+float duration, distance;
 
 void setup() {
   Serial.begin(9600);           
@@ -59,15 +91,54 @@ void setup() {
 
   motorL->setSpeed(vSpeedLeft);
   motorR->setSpeed(vSpeedRight);
+  pincerL->setSpeed(pincer_speed);
+  pincerR->setSpeed(pincer_speed);
   motorL->run(FORWARD);
   motorR->run(BACKWARD);
   // turn on motor
   motorL->run(RELEASE);
   motorR->run(RELEASE);
+  pinMode(7, OUTPUT); // For colour test
+  
+  pinMode(blue_pin, OUTPUT);
+  pinMode(red_pin, OUTPUT);
+  pinMode(amber_pin, OUTPUT);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+//  //amber LED interrupt set up
+//  Timer1.initialize(150000);
+//  Timer1.attachInterrupt(amberLEDInterrupt); // blinkLED to run every 0.15 seconds
+  
 }
+
+
+//amber LED interrrupt
+//void amberLEDInterrupt(void){
+//  if(!hasRedBlock && !hasBlueBlock){
+//    digitalWrite(amber_pin, !digitalRead(amber_pin));
+//  }else{
+//    digitalWrite(amber_pin, LOW);
+//  }
+//}
+
 
 void loop() {
   uint8_t i;
+  LED_Flash();
+  //if a if statement is matched, noAction is set to zero
+  noAction++;
+
+  //avoid blocks
+  //possibly need also include Linefind in the block avoidance 
+  if(hasRedBlock || hasBlueBlock){
+    distanceBlock = analogRead(Front_sensor_pin);
+    if (distanceBlock < 20){
+      blockAvoidance();
+    }
+  }
+  
 
   //basic line following function(finished)
   lineFollowMain();
@@ -80,26 +151,28 @@ void loop() {
 
      delay(10);
      //set motor to turn 90 degrees to right
-     
+     //use OPB line sensor to stop turning right sensor first detects the line and goes off line again
+     //or use magnet sensor to make motor turn for certain number of turns or ultrasonic distance sensor to check distance
     tTurn(0);
     leftSide = false;
   }
 
+
   //Blue block collected and go to the left side
   if(!leftSide){
       if (tunnel_delay = 10){// Dont want to check every loop so difference is clear, but short enough that corners dont trigger
-      if((side_sensor_history - analogRead(side_sensor_pin)) < -450){ // Distance but might have to use voltage
+      if((side_sensor_history - ultrasonicDistance(side_sensor_pin)) < -450){ // Distance but might have to use voltage
         Tunnel_near = true;
         delayRunning = true;
         delayStart = millis(); // Start delay
       }
-        side_sensor_history = analogRead(side_sensor_pin);
+        side_sensor_history = ultrasonicDistance(side_sensor_pin);
         tunnel_delay = 0;
       }else{
         tunnel_delay += 1;
      } 
 
-    if (delayRunning && ((millis() - delayStart) >= 10000)) {
+     if (delayRunning && ((millis() - delayStart) >= 10000)) {
     delayRunning = false; // // prevent this code being run more then once
     Tunnel_near = false;
     }  
@@ -117,6 +190,7 @@ void loop() {
         motorR->run(FORWARD);
         line_detector_history = digitalRead(1); // Actualy = digital read of line sensor
         for(int i = 0;  i < 40; i = i+1){
+           LED_Flash();
            // 8 second delay roughly 90 degrees so go for 10 for 100 degree
            delay(250); // 4 times a second should be ok.
            if(digitalRead(1) != line_detector_history){ // Checks to see if state changes and if it does updates the history
@@ -129,12 +203,12 @@ void loop() {
             motorL->run(RELEASE);
             motorR->run(RELEASE);
            //right turn finished, go to the left side
-           side_sensor_history = analogRead(side_sensor_pin);
+           side_sensor_history = ultrasonicDistance(side_sensor_pin);
             while(true){
             //line follow to the left side of the tunnel
             lineFollowMain();
             //Y turn reached, use side sensor to check the wall of the tunnel 
-            if(analogRead(side_sensor_pin) - side_sensor_history > 300){
+            if(ultrasonicDistance(side_sensor_pin) - side_sensor_history > 300){
               //make the slow right turn
               motorR->setSpeed(100);
               motorL->setSpeed(150);
@@ -142,7 +216,7 @@ void loop() {
               leftSide = true;
               break;
             }else{
-              side_sensor_history = analogRead(side_sensor_pin);
+              side_sensor_history = ultrasonicDistance(side_sensor_pin);
             }
            
           } 
@@ -171,6 +245,7 @@ void loop() {
             tunnel_counter = 0;
             for(int i = 0;  i < 45; i = i+1){
              // 8 second delay roughly 90 degrees so go for 10
+             LED_Flash();
              delay(250); // 4 times a second should be ok.
              if(right_sensor_state != line_detector_history){ // current digital read here
                tunnel_counter +=1;
@@ -186,11 +261,82 @@ void loop() {
     } 
   }
 
+  
+   //T turn on the left after blue block placed
+  if(checkTturnLeft()){
+
+     if(!hasBlueBlock && leftSide){
+      if(true){  //use ultrasonic sensor to estimate location of tunnel
+        if(right_sensor_state == 0){ //arrive at centre of the tunnel
+        //make the right turn
+        // Reverse code here if needed. Assuming not
+        delayPause = millis();// Pause timer
+        motorL->run(RELEASE);
+        motorR->run(RELEASE);
+        motorR->setSpeed(70);
+        motorL->setSpeed(135);
+        motorL->run(FORWARD);
+        motorR->run(FORWARD);
+        line_detector_history = digitalRead(1); // Actualy = digital read of line sensor
+        for(int i = 0;  i < 40; i = i+1){
+           // 8 second delay roughly 90 degrees so go for 10 for 100 degree
+           delay(250); // 4 times a second should be ok.
+           LED_Flash();
+           if(digitalRead(1) != line_detector_history){ // Checks to see if state changes and if it does updates the history
+             tunnel_counter +=1;
+             line_detector_history = digitalRead(1);
+           }
+           if(tunnel_counter == 3){
+            Tunnel_near = true;
+            delayRunning = true;
+            motorL->run(RELEASE);
+            motorR->run(RELEASE);
+           //right turn finished, go to the left side
+           side_sensor_history = ultrasonicDistance(side_sensor_pin);
+           }
+           
+          if(tunnel_counter != 3){
+                      motorR->setSpeed(155);
+                      motorL->setSpeed(70);
+                      motorL->run(BACKWARD);
+                      motorR->run(BACKWARD);
+                      tunnel_counter = 0;
+                      for(int i = 0;  i < 45; i = i+1){
+                       // 8 second delay roughly 90 degrees so go for 10
+                       LED_Flash();
+                       delay(250); // 4 times a second should be ok.
+                       if(right_sensor_state != line_detector_history){ // current digital read here
+                         tunnel_counter +=1;
+                         line_detector_history = digitalRead(left_sensor_pin); // - current digital read
+                       }
+                       if(tunnel_counter == 2){
+                        delayStart = delayPause - delayStart;
+                        break;
+                       }
+                    }
+                   }  
+            }
+         }
+      }
+     }
+  }
 }
 
 // check horizontal line in front of robot, take into account robot may not move in straight line
 boolean checkHorizontalLine(){
   return (left_sensor_state == 1 && right_sensor_state == 1) || (left_sensor_state_prev == 1 && right_sensor_state == 1) || (left_sensor_state == 1 && right_sensor_state_prev == 1);
+}
+
+//use ultrasonic sensor to check tunnel
+boolean checkTunnel(){
+  
+}
+
+//use ultrasonic sensor to check the T turn after blue block is placed on the left
+//so that robot can return to right side
+boolean checkTturnLeft(){
+
+  
 }
 
 
@@ -224,7 +370,7 @@ void tTurn(int i){
         motorL->setSpeed(70);      
         motorL->run(BACKWARD);
         delay(turn_delay);
-        
+        LED_Flash();
         left_sensor_state = digitalRead(left_sensor_pin);
         if(left_sensor_state == 1){
           while(true){
@@ -243,6 +389,8 @@ void tTurn(int i){
 }
 
 void lineFollowMain(){
+//  LED_Flash();
+  
   //read from OPB line sensor
   left_sensor_state = digitalRead(left_sensor_pin);
   right_sensor_state = digitalRead(right_sensor_pin);
@@ -250,7 +398,7 @@ void lineFollowMain(){
   if(left_sensor_state == 1){
     left_sensor_state_prev = 1;
   }else{
-    if(sensor_count_left == 15){
+    if(sensor_count_left == 10){
       left_sensor_state_prev = left_sensor_state;
       sensor_count_left = 0;
     }else{
@@ -258,7 +406,7 @@ void lineFollowMain(){
     }   
   }
 
-  if(right_sensor_state == 15){
+  if(right_sensor_state == 1){
     right_sensor_state_prev = 1;
   }else{
     if(sensor_count_right == 10){
@@ -291,7 +439,7 @@ void lineFollowMain(){
     }
     
   //check if motor move in straight line
-  if((right_sensor_state == 0 && left_sensor_state == 0) || (checkHorizontalLine() && leftSide == false))
+  if(right_sensor_state == 0 && left_sensor_state == 0)
   {
     Serial.println("going forward");
     motorL->run(FORWARD);
@@ -304,4 +452,164 @@ void lineFollowMain(){
     isTurn = false;
     delay(turn_delay);
     }
+}
+
+int ultrasonicDistance(int pin){
+    
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  return (int) distance;
+  
+}
+
+void blockPlacing(){
+    motorL->run(RELEASE); // Stops motors then picks up blocks
+    motorR->run(RELEASE);
+    pincerL->run(FORWARD); // place the blocks
+    pincerR->run(BACKWARD);
+    delay(pincer_delay);
+
+    //turn off red/blue LED
+    LED_Change(RED,false);
+    LED_Change(BLUE,false);
+
+    hasRedBlock = false;
+    hasBlueBlock = false;
+       
+    //block placed, move backward a bit
+    motorL->run(BACKWARD); // Sets motors going backward 
+    motorR->run(FORWARD);
+    delay(200);
+    LED_Flash();
+    motorL->run(FORWARD); // Sets motors going forward
+    motorR->run(BACKWARD);
+}
+
+void blockAvoidance(){
+    // Reverse section?
+    //set up a 90 degree left turn (might make seporate 90 turn functions if reused a lot)
+    motorL->setSpeed(slow_speed);
+    for(int i = 0;  i < 40; i = i+1){
+      delay(100); //BigTurnDelay
+      LED_Flash();
+    }
+    //sets up a streight section
+    motorL->setSpeed(vSpeedLeft);
+    for(int i = 0;  i < 20; i = i+1){
+      delay(100); //BigTurnDelay
+      LED_Flash();
+    }
+    //Turns right and goes forward
+    motorR->setSpeed(slow_speed);
+    for(int i = 0;  i < 40; i = i+1){
+      delay(100); //BigTurnDelay
+      LED_Flash();
+    }
+    motorR->setSpeed(vSpeedRight);
+    boolean EarlyBreak = false;
+    for (int i = 0; i <= 100;i=i+1){
+      delay(30);
+      LED_Flash();
+      if (digitalRead(left_sensor_pin) == 1){//posative read so crosses line in streight around block
+        EarlyBreak = true;
+        break;
+      }
+    }
+    //Turns right again and returns to line location
+    if (not EarlyBreak){
+      motorR->setSpeed(slow_speed);
+      for(int i = 0;  i < 40; i = i+1){
+        delay(100); //BigTurnDelay
+        LED_Flash();
+        }
+      motorR->setSpeed(vSpeedRight);
+    }
+   if (digitalRead(left_sensor_pin) == 1){//might need to add a reverse here //Also is 1 white or black. This is assuming white
+    motorL->run(BACKWARD);
+    for(int i = 0;  i < 40; i = i+1){ // Delay is shot in the dark
+        delay(100); //BigTurnDelay
+        LED_Flash();
+        }
+    motorL->run(FORWARD);
+   }
+//Hopefully this works. Will have to adjust the timings and speeds
+  
+}
+
+
+
+
+
+void blockPickUp(){ // Assuming this is also used for ditection
+  distanceBlock = analogRead(Front_sensor_pin);
+  if(distanceBlock < 20 && not hasBlueBlock && not hasRedBlock){ // Unlikly distance block is correct
+      motorL->run(RELEASE); // Stops motors then picks up blocks
+      motorR->run(RELEASE);
+      pincerL->run(pincer_speed); // Picks up blocks
+      pincerR->run(pincer_speed);
+      delay(pincer_delay);
+      digitalWrite(7, HIGH); // turn blue LED on
+      // Might need a delay
+      sensorValue0 = analogRead(sensorPin0);
+//  Serial.println(sensorValue0);
+  
+      sensorValue1 = analogRead(sensorPin1);
+ // Serial.println(sensorValue1);
+  
+      sensorValue2 = analogRead(sensorPin2);
+ // Serial.println(sensorValue2);
+      if ((sensorValue1) > sensorValue2){
+        LED_Change(RED,true);
+        hasRedBlock = true;
+      }
+      else{
+        LED_Change(BLUE,true);
+        hasBlueBlock = true;
+      }
+
+      Serial.print(red_blue);
+      motorL->run(FORWARD); // Sets motors going forward again
+      motorR->run(BACKWARD);
+  }
+}
+
+// Function to change the LED's colour - quick and dirty improve over weekend
+void LED_Change(int colour, boolean ONf){
+ if (colour == RED){
+  current_LED = red_pin;
+ }
+ if (colour == BLUE){
+  current_LED = blue_pin;
+ }
+ if(ONf){
+  digitalWrite(current_LED, HIGH);
+ }
+ else{
+  digitalWrite(current_LED, LOW);
+ } 
+}
+
+// Function to make the LED flash - if there is a long sub function will need to add this to that to
+void LED_Flash(){
+  if(!hasRedBlock && !hasBlueBlock){
+    if(millis() - two_hz_delay >= 250){
+    if(AmberON){
+      digitalWrite(amber_pin, LOW);
+      Serial.println("OFF");
+      AmberON = false;
+    }
+    else{
+      digitalWrite(amber_pin, HIGH);
+      Serial.println("ON");
+      AmberON = true;
+    } 
+    two_hz_delay = millis();
+  }    
+  }
 }
